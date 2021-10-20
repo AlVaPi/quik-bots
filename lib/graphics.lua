1d059247a959639277f21f1827dfa51cab8dc51a
Alexander <elixpc@gmail.com>
2021-10-20 20:50:19 +0300

    Rename graphics.lua to lib/graphics.lua

--- lib/graphics.lua
+++ lib/graphics.lua
@@ -2,17 +2,17 @@
 Graphics functions
 ]]
 function isChartExist(chart_name)
-	-- возвращает true, если график с идентификатором chart_name сущестует иначе false
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ true, ГҐГ±Г«ГЁ ГЈГ°Г ГґГЁГЄ Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name Г±ГіГ№ГҐГ±ГІГіГҐГІ ГЁГ­Г Г·ГҐ false
 	if chart_name==nil or chart_name=='' then return false end
 	local n=getNumCandles(chart_name)
 	if n==nil or n<1 then return false end
 	return true
 end
 function getCandle(chart_name,bar,line)
-	-- возвращает свечу со сдвигом bar от последней существующей для графика с идентификатором chart_name
-	-- параметр line не обязательный (по умолчанию 0)
-	-- параметр bar не обязательный (по умолчанию 0)
-	-- возвращает таблицу Луа с запришиваемой свечей или nil и сообщение с диагностикой
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ Г±ГўГҐГ·Гі Г±Г® Г±Г¤ГўГЁГЈГ®Г¬ bar Г®ГІ ГЇГ®Г±Г«ГҐГ¤Г­ГҐГ© Г±ГіГ№ГҐГ±ГІГўГіГѕГ№ГҐГ© Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name
+	-- ГЇГ Г°Г Г¬ГҐГІГ° line Г­ГҐ Г®ГЎГїГ§Г ГІГҐГ«ГјГ­Г»Г© (ГЇГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ 0)
+	-- ГЇГ Г°Г Г¬ГҐГІГ° bar Г­ГҐ Г®ГЎГїГ§Г ГІГҐГ«ГјГ­Г»Г© (ГЇГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ 0)
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГІГ ГЎГ«ГЁГ¶Гі Г‹ГіГ  Г± Г§Г ГЇГ°ГЁГёГЁГўГ ГҐГ¬Г®Г© Г±ГўГҐГ·ГҐГ© ГЁГ«ГЁ nil ГЁ Г±Г®Г®ГЎГ№ГҐГ­ГЁГҐ Г± Г¤ГЁГ ГЈГ­Г®Г±ГІГЁГЄГ®Г©
 	if not isChartExist(chart_name) then return nil,'Chart doesn`t exist' end
 	local n=getNumCandles(chart_name)
 	local lline=0
@@ -24,17 +24,17 @@ function getCandle(chart_name,bar,line)
 	if t~=nil and n>=1 and t[0]~=nil and t[0].doesExist==1 then return t[0] else return nil,'Error gettind Candles from '..chart_name end
 end
 function getPrevCandle(chart_name,line)
-	-- возвращает пред-последнюю свечу для графика с идентификатором chart_name
-	-- параметр line не обязательный (по умолчанию 0)
-	-- возвращает таблицу Луа с запришиваемой свечей или nil и сообщение с диагностикой
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГЇГ°ГҐГ¤-ГЇГ®Г±Г«ГҐГ¤Г­ГѕГѕ Г±ГўГҐГ·Гі Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name
+	-- ГЇГ Г°Г Г¬ГҐГІГ° line Г­ГҐ Г®ГЎГїГ§Г ГІГҐГ«ГјГ­Г»Г© (ГЇГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ 0)
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГІГ ГЎГ«ГЁГ¶Гі Г‹ГіГ  Г± Г§Г ГЇГ°ГЁГёГЁГўГ ГҐГ¬Г®Г© Г±ГўГҐГ·ГҐГ© ГЁГ«ГЁ nil ГЁ Г±Г®Г®ГЎГ№ГҐГ­ГЁГҐ Г± Г¤ГЁГ ГЈГ­Г®Г±ГІГЁГЄГ®Г©
 	if not isChartExist(chart_name) then return nil,'Chart doesn`t exist' end
 	local n=getNumCandles(chart_name)
 	return getCandle(chart_name,1,line)
 end
 function getLastCandle(chart_name,line)
-	-- возвращает последнюю свечу для графика с идентификатором chart_name
-	-- параметр line не обязательный (по умолчанию 0)
-	-- возвращает таблицу Луа с запришиваемой свечей или nil и сообщение с диагностикой
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГЇГ®Г±Г«ГҐГ¤Г­ГѕГѕ Г±ГўГҐГ·Гі Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name
+	-- ГЇГ Г°Г Г¬ГҐГІГ° line Г­ГҐ Г®ГЎГїГ§Г ГІГҐГ«ГјГ­Г»Г© (ГЇГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ 0)
+	-- ГўГ®Г§ГўГ°Г Г№Г ГҐГІ ГІГ ГЎГ«ГЁГ¶Гі Г‹ГіГ  Г± Г§Г ГЇГ°ГЁГёГЁГўГ ГҐГ¬Г®Г© Г±ГўГҐГ·ГҐГ© ГЁГ«ГЁ nil ГЁ Г±Г®Г®ГЎГ№ГҐГ­ГЁГҐ Г± Г¤ГЁГ ГЈГ­Г®Г±ГІГЁГЄГ®Г©
 	return getCandle(chart_name,nil,line)
 end
 --[[
@@ -42,9 +42,9 @@ Commmon Trading Signals
 ]]
 
 function CrossOver(bar,chart_name1,val2,parameter,line1,line2)
-	-- Возвращает true если график с идентификатором chart_name1 пересек снизу вверх график (или значение) val2 в баре со смещением bar.
-	-- параметры parameter,line1,line2 необязательны. По умолчания равны close,0,0 соответственно
-	-- вторым параметром возвращается точка пересечения (цена)
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ true ГҐГ±Г«ГЁ ГЈГ°Г ГґГЁГЄ Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name1 ГЇГҐГ°ГҐГ±ГҐГЄ Г±Г­ГЁГ§Гі ГўГўГҐГ°Гµ ГЈГ°Г ГґГЁГЄ (ГЁГ«ГЁ Г§Г­Г Г·ГҐГ­ГЁГҐ) val2 Гў ГЎГ Г°ГҐ Г±Г® Г±Г¬ГҐГ№ГҐГ­ГЁГҐГ¬ bar.
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line1,line2 Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГї Г°Г ГўГ­Г» close,0,0 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
+	-- ГўГІГ®Г°Г»Г¬ ГЇГ Г°Г Г¬ГҐГІГ°Г®Г¬ ГўГ®Г§ГўГ°Г Г№Г ГҐГІГ±Гї ГІГ®Г·ГЄГ  ГЇГҐГ°ГҐГ±ГҐГ·ГҐГ­ГЁГї (Г¶ГҐГ­Г )
 	if bar==nil or chart_name1==nil or val2==nil then return false,'Bad parameters' end
 	local candle1l=getCandle(chart_name1,bar,line1)
 	local candle1p=getCandle(chart_name1,bar+1,line1)
@@ -67,9 +67,9 @@ function CrossOver(bar,chart_name1,val2,parameter,line1,line2)
 	end
 end
 function CrossUnder(bar,chart_name1,val2,parameter,line1,line2)
-	-- Возвращает true если график с идентификатором chart_name1 пересек сверху вниз  график (или значение) val2 в баре bar.
-	-- параметры parameter,line1,line2 необязательны. По умолчания равны close,0,0 соответственно
-	-- вторым параметром возвращается точка пересечения (цена), если есть пересечение
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ true ГҐГ±Г«ГЁ ГЈГ°Г ГґГЁГЄ Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name1 ГЇГҐГ°ГҐГ±ГҐГЄ Г±ГўГҐГ°ГµГі ГўГ­ГЁГ§  ГЈГ°Г ГґГЁГЄ (ГЁГ«ГЁ Г§Г­Г Г·ГҐГ­ГЁГҐ) val2 Гў ГЎГ Г°ГҐ bar.
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line1,line2 Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГї Г°Г ГўГ­Г» close,0,0 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
+	-- ГўГІГ®Г°Г»Г¬ ГЇГ Г°Г Г¬ГҐГІГ°Г®Г¬ ГўГ®Г§ГўГ°Г Г№Г ГҐГІГ±Гї ГІГ®Г·ГЄГ  ГЇГҐГ°ГҐГ±ГҐГ·ГҐГ­ГЁГї (Г¶ГҐГ­Г ), ГҐГ±Г«ГЁ ГҐГ±ГІГј ГЇГҐГ°ГҐГ±ГҐГ·ГҐГ­ГЁГҐ
 	if bar==nil or chart_name1==nil or val2==nil then return false,'Bad parameters' end
 	local candle1l=getCandle(chart_name1,bar,line1)
 	local candle1p=getCandle(chart_name1,bar+1,line1)
@@ -95,8 +95,8 @@ function CrossUnder(bar,chart_name1,val2,parameter,line1,line2)
 	end
 end
 function TrendDown(bar,chart_name,parameter,line)
-	-- Возвращает true если график с идентификатором chart_name "развернулся вниз". Т.е. значение графика в баре bar меньше значения в баре bar-1.
-	-- параметры parameter,line необязательны. По умолчания равны close,0 соответственно
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ true ГҐГ±Г«ГЁ ГЈГ°Г ГґГЁГЄ Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name "Г°Г Г§ГўГҐГ°Г­ГіГ«Г±Гї ГўГ­ГЁГ§". Г’.ГҐ. Г§Г­Г Г·ГҐГ­ГЁГҐ ГЈГ°Г ГґГЁГЄГ  Гў ГЎГ Г°ГҐ bar Г¬ГҐГ­ГјГёГҐ Г§Г­Г Г·ГҐГ­ГЁГї Гў ГЎГ Г°ГҐ bar-1.
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГї Г°Г ГўГ­Г» close,0 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
 	if bar==nil or chart_name==nil then return false,'Bad parameters' end
 	local candle1l,candle1p=getCandle(chart_name,bar,line),getCandle(chart_name,bar+1,line)
 	if candle1l==nil or candle1p==nil then return false,'Eror on getting candles for '..chart_name end
@@ -104,8 +104,8 @@ function TrendDown(bar,chart_name,parameter,line)
 	if candle1l[par]<candle1p[par] then return true else return false end
 end
 function TrendUp(bar,chart_name,parameter,line)
-	-- Возвращает true если график с идентификатором chart_name "развернулся вверх". Т.е. значение графика в баре bar больше значения в баре bar-1.
-	-- параметры parameter,line необязательны. По умолчания равны close,0 соответственно
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ true ГҐГ±Г«ГЁ ГЈГ°Г ГґГЁГЄ Г± ГЁГ¤ГҐГ­ГІГЁГґГЁГЄГ ГІГ®Г°Г®Г¬ chart_name "Г°Г Г§ГўГҐГ°Г­ГіГ«Г±Гї ГўГўГҐГ°Гµ". Г’.ГҐ. Г§Г­Г Г·ГҐГ­ГЁГҐ ГЈГ°Г ГґГЁГЄГ  Гў ГЎГ Г°ГҐ bar ГЎГ®Г«ГјГёГҐ Г§Г­Г Г·ГҐГ­ГЁГї Гў ГЎГ Г°ГҐ bar-1.
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГї Г°Г ГўГ­Г» close,0 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
 	if bar==nil or chart_name==nil then return false,'Bad parameters' end
 	local candle1l,candle1p=getCandle(chart_name,bar,line),getCandle(chart_name,bar+1,line)
 	if candle1l==nil or candle1p==nil then return false,'Eror on getting candles for '..chart_name end
@@ -136,8 +136,8 @@ function CandleDiff(chart_name,bar,line)
 	return candle["close"] - candle["open"] 
 end
 function Average(bar,chart_name,number,parameter,line)
-	-- Возвращает среднее арифметическое number свечей до bar свечи
-	-- параметры parameter,line,number необязательны. По умолчанию равны close,0,2 соответственно
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ Г±Г°ГҐГ¤Г­ГҐГҐ Г Г°ГЁГґГ¬ГҐГІГЁГ·ГҐГ±ГЄГ®ГҐ number Г±ГўГҐГ·ГҐГ© Г¤Г® bar Г±ГўГҐГ·ГЁ
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line,number Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ Г°Г ГўГ­Г» close,0,2 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
 	if bar==nil or chart_name==nil then return false,'Bad parameters' end
 --	local candle1l,candle1p=getCandle(chart_name,bar,line),getCandle(chart_name,bar+1,line)
 --	if candle1l==nil or candle1p==nil then return false,'Eror on getting candles for '..chart_name end
@@ -153,8 +153,8 @@ function Average(bar,chart_name,number,parameter,line)
 	return aver
 end
 function MinMax(bar,chart_name,number,parameter,line)
-	-- Возвращает минимум, максимум и среднее number свечей до bar свечи
-	-- параметры parameter,line,number необязательны. По умолчанию равны close,0,2 соответственно
+	-- Г‚Г®Г§ГўГ°Г Г№Г ГҐГІ Г¬ГЁГ­ГЁГ¬ГіГ¬, Г¬Г ГЄГ±ГЁГ¬ГіГ¬ ГЁ Г±Г°ГҐГ¤Г­ГҐГҐ number Г±ГўГҐГ·ГҐГ© Г¤Г® bar Г±ГўГҐГ·ГЁ
+	-- ГЇГ Г°Г Г¬ГҐГІГ°Г» parameter,line,number Г­ГҐГ®ГЎГїГ§Г ГІГҐГ«ГјГ­Г». ГЏГ® ГіГ¬Г®Г«Г·Г Г­ГЁГѕ Г°Г ГўГ­Г» close,0,2 Г±Г®Г®ГІГўГҐГІГ±ГІГўГҐГ­Г­Г®
 	if bar==nil or chart_name==nil then return false,'Bad parameters' end
 --	local candle1l,candle1p=getCandle(chart_name,bar,line),getCandle(chart_name,bar+1,line)
 --	if candle1l==nil or candle1p==nil then return false,'Eror on getting candles for '..chart_name end
@@ -179,34 +179,34 @@ function MinMax(bar,chart_name,number,parameter,line)
 end
 
 function PlaceLabel(chart_tag, price, name, text, date_pos, time_pos)
-	-- Внимание, название всех параметров должны писаться большими буквами
+	-- Г‚Г­ГЁГ¬Г Г­ГЁГҐ, Г­Г Г§ГўГ Г­ГЁГҐ ГўГ±ГҐГµ ГЇГ Г°Г Г¬ГҐГІГ°Г®Гў Г¤Г®Г«Г¦Г­Г» ГЇГЁГ±Г ГІГјГ±Гї ГЎГ®Г«ГјГёГЁГ¬ГЁ ГЎГіГЄГўГ Г¬ГЁ
 	label_params = {
-		-- Если подпись не требуется то оставить строку пустой ""
+		-- Г…Г±Г«ГЁ ГЇГ®Г¤ГЇГЁГ±Гј Г­ГҐ ГІГ°ГҐГЎГіГҐГІГ±Гї ГІГ® Г®Г±ГІГ ГўГЁГІГј Г±ГІГ°Г®ГЄГі ГЇГіГ±ГІГ®Г© ""
 		TEXT = text or "",
-		-- Расположение картинки относительно текста (возможно 4 варианта: LEFT, RIGHT, TOP, BOTTOM)
+		-- ГђГ Г±ГЇГ®Г«Г®Г¦ГҐГ­ГЁГҐ ГЄГ Г°ГІГЁГ­ГЄГЁ Г®ГІГ­Г®Г±ГЁГІГҐГ«ГјГ­Г® ГІГҐГЄГ±ГІГ  (ГўГ®Г§Г¬Г®Г¦Г­Г® 4 ГўГ Г°ГЁГ Г­ГІГ : LEFT, RIGHT, TOP, BOTTOM)
 		ALIGNMENT = "LEFT",
-		-- Значение параметра на оси Y, к которому будет привязана метка
+		-- Г‡Г­Г Г·ГҐГ­ГЁГҐ ГЇГ Г°Г Г¬ГҐГІГ°Г  Г­Г  Г®Г±ГЁ Y, ГЄ ГЄГ®ГІГ®Г°Г®Г¬Гі ГЎГіГ¤ГҐГІ ГЇГ°ГЁГўГїГ§Г Г­Г  Г¬ГҐГІГЄГ 
 		YVALUE = price,
-		-- Дата в формате «ГГГГММДД», к которой привязана метка
+		-- Г„Г ГІГ  Гў ГґГ®Г°Г¬Г ГІГҐ В«ГѓГѓГѓГѓГЊГЊГ„Г„В», ГЄ ГЄГ®ГІГ®Г°Г®Г© ГЇГ°ГЁГўГїГ§Г Г­Г  Г¬ГҐГІГЄГ 
 		DATE = date_pos or os.date("%Y%m%d"),
-		-- Время в формате «ЧЧММСС», к которому будет привязана метка
+		-- Г‚Г°ГҐГ¬Гї Гў ГґГ®Г°Г¬Г ГІГҐ В«Г—Г—ГЊГЊГ‘Г‘В», ГЄ ГЄГ®ГІГ®Г°Г®Г¬Гі ГЎГіГ¤ГҐГІ ГЇГ°ГЁГўГїГ§Г Г­Г  Г¬ГҐГІГЄГ 
 		TIME = time_pos or os.date("%H%M%S",os.time()),
-		-- Красная компонента цвета в формате RGB. Число в интервале [0;255]
+		-- ГЉГ°Г Г±Г­Г Гї ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ  Г¶ГўГҐГІГ  Гў ГґГ®Г°Г¬Г ГІГҐ RGB. Г—ГЁГ±Г«Г® Гў ГЁГ­ГІГҐГ°ГўГ Г«ГҐ [0;255]
 		R = 100,
-		-- Зеленая компонента цвета в формате RGB. Число в интервале [0;255]
+		-- Г‡ГҐГ«ГҐГ­Г Гї ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ  Г¶ГўГҐГІГ  Гў ГґГ®Г°Г¬Г ГІГҐ RGB. Г—ГЁГ±Г«Г® Гў ГЁГ­ГІГҐГ°ГўГ Г«ГҐ [0;255]
 		G = 200,
-		-- Синяя компонента цвета в формате RGB. Число в интервале [0;255]
+		-- Г‘ГЁГ­ГїГї ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ  Г¶ГўГҐГІГ  Гў ГґГ®Г°Г¬Г ГІГҐ RGB. Г—ГЁГ±Г«Г® Гў ГЁГ­ГІГҐГ°ГўГ Г«ГҐ [0;255]
 		B = 80,
-		-- Прозрачность метки в процентах. Значение должно быть в промежутке [0; 100]
+		-- ГЏГ°Г®Г§Г°Г Г·Г­Г®Г±ГІГј Г¬ГҐГІГЄГЁ Гў ГЇГ°Г®Г¶ГҐГ­ГІГ Гµ. Г‡Г­Г Г·ГҐГ­ГЁГҐ Г¤Г®Г«Г¦Г­Г® ГЎГ»ГІГј Гў ГЇГ°Г®Г¬ГҐГ¦ГіГІГЄГҐ [0; 100]
 		TRANSPARENCY = 0,
-		-- Прозрачность фона картинки. Возможные значения: «0» – прозрачность отключена, «1» – прозрачность включена
+		-- ГЏГ°Г®Г§Г°Г Г·Г­Г®Г±ГІГј ГґГ®Г­Г  ГЄГ Г°ГІГЁГ­ГЄГЁ. Г‚Г®Г§Г¬Г®Г¦Г­Г»ГҐ Г§Г­Г Г·ГҐГ­ГЁГї: В«0В» В– ГЇГ°Г®Г§Г°Г Г·Г­Г®Г±ГІГј Г®ГІГЄГ«ГѕГ·ГҐГ­Г , В«1В» В– ГЇГ°Г®Г§Г°Г Г·Г­Г®Г±ГІГј ГўГЄГ«ГѕГ·ГҐГ­Г 
 		TRANSPARENT_BACKGROUND = 1,
-		-- Название шрифта (например «Arial»)
+		-- ГЌГ Г§ГўГ Г­ГЁГҐ ГёГ°ГЁГґГІГ  (Г­Г ГЇГ°ГЁГ¬ГҐГ° В«ArialВ»)
 		FONT_FACE_NAME = "Arial",
-		-- Размер шрифта
+		-- ГђГ Г§Г¬ГҐГ° ГёГ°ГЁГґГІГ 
 		FONT_HEIGHT = 12,
-		-- Текст всплывающей подсказки
-		HINT = name.." по цене "..price
+		-- Г’ГҐГЄГ±ГІ ГўГ±ГЇГ«Г»ГўГ ГѕГ№ГҐГ© ГЇГ®Г¤Г±ГЄГ Г§ГЄГЁ
+		HINT = name.." ГЇГ® Г¶ГҐГ­ГҐ "..price
 	}
 	if name == "open long" or name == "trend up" then
 		label_params.IMAGE_PATH = getScriptPath() .. "\\images\\buy.bmp"
@@ -214,6 +214,6 @@ function PlaceLabel(chart_tag, price, name, text, date_pos, time_pos)
 		label_params.IMAGE_PATH = getScriptPath() .. "\\images\\sell.bmp"
 		
 	end
-	-- Добавляем метку и запоминаем ее ID
+	-- Г„Г®ГЎГ ГўГ«ГїГҐГ¬ Г¬ГҐГІГЄГі ГЁ Г§Г ГЇГ®Г¬ГЁГ­Г ГҐГ¬ ГҐГҐ ID
 	label_id = AddLabel(chart_tag, label_params)
 end
